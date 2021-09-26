67275e360a850c243b0bf5fb3aff9e44243b9d14
lavsam-dev <lavsam@gmail.com>
2021-09-26 15:24:56 +0400

    Меню в BottomNavigationDrawerFragment.kt с вызовом всех активностей для animations + layout for earth recycler

--- app/src/main/java/com/learn/lavsam/mymaterial/ui/picture/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/learn/lavsam/mymaterial/ui/picture/BottomNavigationDrawerFragment.kt
@@ -1,12 +1,13 @@
 package com.learn.lavsam.mymaterial.ui.picture
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.learn.lavsam.mymaterial.R
+import com.learn.lavsam.mymaterial.ui.animations.*
 import kotlinx.android.synthetic.main.bottom_navigation_layout.*
 
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
@@ -24,9 +25,35 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
-                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                R.id.navigation_one -> activity?.let {
+                    startActivity(Intent(it, AnimationsActivity::class.java))
+                }
+                R.id.navigation_two -> activity?.let {
+                    startActivity(Intent(it, AnimaBonusActivity::class.java))
+                }
+                R.id.navigation_three -> activity?.let {
+                    startActivity(Intent(it, AnimaEnlargeActivity::class.java))
+                }
+                R.id.navigation_four -> activity?.let {
+                    startActivity(Intent(it, AnimaExplodeActivity::class.java))
+                }
+                R.id.navigation_five -> activity?.let {
+                    startActivity(Intent(it, AnimaFabActivity::class.java))
+                }
+                R.id.navigation_six -> activity?.let {
+                    startActivity(Intent(it, AnimaPathActivity::class.java))
+                }
+                R.id.navigation_seven -> activity?.let {
+                    startActivity(Intent(it, AnimaShuffleActivity::class.java))
+                }
+                R.id.navigation_eight -> activity?.let {
+                    startActivity(Intent(it, AnimaStateListActivity::class.java))
+                }
+                R.id.navigation_nine -> activity?.let {
+                    startActivity(Intent(it, AnimaVisibilityActivity::class.java))
+                }
             }
+            dismiss()
             true
         }
     }
